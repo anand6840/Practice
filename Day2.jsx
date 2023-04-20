ad9c031f2012f886b8ea34bd71a43b474fd1ac8a
Anand Waghmare <103956874+anand6840@users.noreply.github.com>
2023-04-20 23:11:01 +0530

    counter app added

---
+++ Day2.jsx
@@ -0,0 +1,29 @@
+// Counter App in React
+
+import React from 'react'
+import { useState } from 'react'
+
+
+
+function App() {
+	
+    const [counter, setCounter] = useState(0)
+
+    function increment(){
+        setCounter ( counter+1)
+    }
+
+     function decrement(){
+        setCounter ( counter-1)
+    }
+
+	return (
+		<div className="App">
+			<h1>Counter: <span>{counter}</span></h1>
+			<button onClick={increment}>Increment</button>
+			<button onClick={decrement}>Decrement</button>
+		</div>
+	)
+}
+
+export default App
